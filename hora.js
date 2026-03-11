4bde463a8245802c3aa763f2615556854c5b4ad3
wellingtim-dts <wellington.santos.soares@escola.pr.gov.br>
2026-03-11 12:45:03 +0000

    Minha primeira atualização

--- hora.js
+++ hora.js
@@ -1,1 +1,12 @@
+// Valores iniciais do dia
+let valorPorHora = 50;
+let horasTrabalhadas = 8; // Expediente padrão
 
+console.log("--- Resumo do Expediente ---");
+console.log(`Horas iniciais: ${horasTrabalhadas}h`);
+
+horasTrabalhadas++;
+console.log(`Após hora extra (horasTrabalhadas++): ${horasTrabalhadas}h`);
+
+horasTrabalhadas--;
+console.log('após saída antecipada: ${horasTrabalhasdas}h')
