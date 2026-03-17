b9cf70b375b3b14f2c1220a4672e56726eb8991c
wellingtim-dts <wellington.santos.soares@escola.pr.gov.br>
2026-03-17 11:51:25 +0000

    1

---
+++ stringo.js
@@ -0,0 +1,12 @@
+console.log("ENDEREÇO COMPLETO");
+
+let rua = "Rua das Flores";
+let numero = 9851;
+let cidade = "Curitiba";
+let cep = "80010-000";
+
+let enderecoCompleto = rua + "," + numero + "-" + cidade;
+
+console.log(enderecoCompleto);
+
+//Adicione informações adicionais
