7b56495831b66a601534031b1e077586bf275732
wellingtim-dts <wellington.santos.soares@escola.pr.gov.br>
2026-03-17 13:45:48 +0000

    1

---
+++ temperatura.js
@@ -0,0 +1,10 @@
+//converção de temperatura
+
+const cidade = "Curitiba";
+const temCelsius = 18.5;
+
+const tempFahrenheit = tempCelsius * (9/5) + 32;
+const tempKelvin = tempCelsius + 273.15;
+const variacoDiaria = 26.3 - tempCelsius;
+
+console.log(Temperatura em ${cidade} hoje está ${tempCelsius}.`);
