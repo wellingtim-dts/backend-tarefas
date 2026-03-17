102dda8d02a06e84a789acf68f36deb9c465bf1d
wellingtim-dts <wellington.santos.soares@escola.pr.gov.br>
2026-03-17 11:34:48 +0000

    1

---
+++ relatorio.js
@@ -0,0 +1,11 @@
+//Relatório de notas
+console.log("Relatório de Notas");
+
+let aluno = "wellington";
+let nota1 = 11;
+let nota2 = 12;
+let nota3 = 13;
+let media = (nota1 + nota2 + nota3) / 3;
+
+//let relatorio = "Aluno: + aluno + "\n" + "Notas: " + nota1 + ", " + nota2 + ", " + nota3;
+let relatorio = "Aluno: "+ aluno + "| Notas: " + nota1 + " e " + nota2 + "| Média: "+ media;
