837da99e7fac5cc8204f18da83fa65622e8e6f5c
wellingtim-dts <wellington.santos.soares@escola.pr.gov.br>
2026-03-23 13:51:25 +0000

    temperatura.js

--- temperatura.js
+++ temperatura.js
@@ -4,7 +4,11 @@ const cidade = "Curitiba";
 const temCelsius = 18.5;
 
 const tempFahrenheit = tempCelsius * (9/5) + 32;
-const tempKelvin = tempCelsius + 273.15;
-const variacoDiaria = 26.3 - tempCelsius;
+const tempKelvin = tempCelsius + 273.15const variacoDiaria = 26.3 - tempCelsius;
 
-console.log(Temperatura em ${cidade} hoje está ${tempCelsius}.`);
+console.log(`\nCelsius: ${tempCelsius} Cº`);
+console.log(`Fahrenheit: ${tempFahrenheit.toFixed(1)} Fº`);
+console.log(`Kelvin: ${tempKelvin} Kº`);
+console.log(`\nVariação prevista: ${variacaoDiaria.toFixed(1)} Cº`);
+
+// Tecla Alt Gr + tecla colchetes para fazer o º
