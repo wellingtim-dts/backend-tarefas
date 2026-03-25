efb32ac87d8851f019d2f6db2acc4a8d9fe8cf46
wellingtim-dts <wellington.santos.soares@escola.pr.gov.br>
2026-03-25 12:39:52 +0000

    verção1

---
+++ idade2.js
@@ -0,0 +1,10 @@
+const nome = "Ana";
+const idade = 18;
+
+if (idade >= 18) {
+    console.log(nome + " é maior de idade.");
+} else if (idade === 17) {
+    console.log(nome + " tem 17 anos - quase lá!");
+} else {
+    console.log(nome + " é menor de idade.");
+}
