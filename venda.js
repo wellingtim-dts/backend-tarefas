ecdbedbb28c390d1083e932eb4a4b664aa1b8dfb
wellingtim-dts <wellington.santos.soares@escola.pr.gov.br>
2026-03-31 14:41:58 +0000

    verção1

--- venda.js
+++ venda.js
@@ -5,3 +5,10 @@ let nomeProduto = "feijão com farinha"
 let preçoProduto = 10.00;
 let estoque = 7;
 let quantidadePedido = 6;
+
+// tem estoque suficiente?
+let estoqueOk = quantidaPedido <= estoque;
+
+if (!estoqueOk) {
+    console.log(" PEDIDO RECUSADO - ESTOQUE INSUFICIENTE ")
+}
