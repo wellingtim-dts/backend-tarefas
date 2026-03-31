a47834fda86316a49692e76e605436778febb23a
wellingtim-dts <wellington.santos.soares@escola.pr.gov.br>
2026-03-31 14:32:30 +0000

    verção1

--- cnh.js
+++ cnh.js
@@ -1,3 +1,16 @@
 const idade = 15;
 const maiorIdade = idade >= 18;
 const possuiCNH = false;
+
+const idade = 17;
+const Maioridade = idade>= 18;
+const possuiCNH = false;
+
+const podeDirigir = Maioridade && possuiCNH;
+console.log('Pode viajar sozinho?', podeDirigir)
+
+const podeViajarSozinho = Maioridade || possuiCNH;
+console.log ('Pode viajar sozinho?', podeViajarSozinho);
+
+const precisaDeAcompanhante = !Maioridade
+console.log('Preciso de Acompanhante?', precisaDeAcompanhante);
---
+++ venda.js
@@ -0,0 +1,7 @@
+let nomeCliente = "jhonatas"
+let tipoCliente = "pobre"
+
+let nomeProduto = "feijão com farinha"
+let preçoProduto = 10.00;
+let estoque = 7;
+let quantidadePedido = 6;
