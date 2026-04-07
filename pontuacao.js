c0bede0a06910dcab52a9211ec51b91861e92235
wellingtim-dts <wellington.santos.soares@escola.pr.gov.br>
2026-04-07 11:51:14 +0000

    vercao3

---
+++ pontuacao.js
@@ -0,0 +1,12 @@
+// sistema de pontuação de um jogo
+const nomeJogador = "Alisa lanches";
+let faseAtual = 3;
+let vidaRestantes = 2;
+
+let pontosPorMissão = [85, 90, 60, 100];
+
+//pontosPorMissão.length
+pontuaçãoTotal = 0;
+
+console.log(`seja bem vinda ${nomeJogador}!`)
+console.log(pontosPorMissão);
