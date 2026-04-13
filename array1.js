758693d524cd98f4b826bacc77099b38a8ae9a83
wellingtim-dts <wellington.santos.soares@escola.pr.gov.br>
2026-04-13 11:53:48 +0000

    vercao1

---
+++ array1.js
@@ -0,0 +1,12 @@
+// de frutas array - ARRAY
+
+const { cloneElement } = require("react");
+
+const frutas =["maçã","Banana","uva", "manga"];
+
+
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);
+console.log(frutas.length);
+frutas[1] = ""
