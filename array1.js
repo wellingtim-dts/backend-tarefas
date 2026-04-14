e23b5f642d650cc5c3c3fd1eadd5201dcf370994
wellingtim-dts <wellington.santos.soares@escola.pr.gov.br>
2026-04-14 11:46:31 +0000

    verçao1

--- array1.js
+++ array1.js
@@ -9,4 +9,10 @@ console.log(frutas[1]);
 console.log(frutas[2]);
 console.log(frutas[3]);
 console.log(frutas.length);
-frutas[1] = ""
+frutas[1] = "morango"; //altera o valor do elemento
+
+
+frutas[3] = "Abacate";
+frutas.push("melão");
+console.log(frutas);
+
