4b0e55f35633cfc2395979efdc09871093a41363
wellingtim-dts <wellington.santos.soares@escola.pr.gov.br>
2026-04-29 12:10:37 +0000

    vercao2

---
+++ objeto.js
@@ -0,0 +1,21 @@
+const bancoAutomotivo = {
+    material: "Couro Natural",
+    cor: "Caramelo",
+    tamanho: "Padrão",
+    concluido: false
+}
+
+console.log("Material escolhido: ", bancoAutomotivo);
+
+//bancoAutomotivo.concluido = true;
+//console.log(bancoAutomotivo);
+
+
+const Moletom = {
+   material: "Políester"
+   cor: "Preto"
+   tamanho: "Grande"
+   concluido: false
+}
+
+console.log
